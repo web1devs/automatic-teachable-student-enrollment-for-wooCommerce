603b4923d4ddcbeecfcabde053d78d3134bfd360
Md. Mahmudul Hasan <greensabuj350@gmail.com>
2023-04-02 18:14:52 +0600

    Update wx-Teachable.php
    Prefix added

--- wx-Teachable.php
+++ wx-Teachable.php
@@ -20,17 +20,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 }
 
-define( 'TCM_ACC_URL', WP_PLUGIN_URL . '/' . plugin_basename( dirname( __FILE__ ) ) . '/' );
-define( 'TCM_ACC_PATH', plugin_dir_path( __FILE__ ) );
+define( 'ATSEW_TCM_ACC_URL', WP_PLUGIN_URL . '/' . plugin_basename( dirname( __FILE__ ) ) . '/' );
+define( 'ATSEW_TCM_ACC_PATH', plugin_dir_path( __FILE__ ) );
 
-$apiKeyShow = get_option('teachable_fild_teachable_api_key');
+$atsew_apiKeyShow = get_option('teachable_fild_teachable_api_key');
 
-define("TEACHABLEAPIKEY", $apiKeyShow );  
-define("PLUGIN_BASENAME",plugin_basename(__FILE__));
+define("ATSEW_TEACHABLEAPIKEY", $atsew_apiKeyShow );  
+define("ATSEW_PLUGIN_BASENAME",plugin_basename(__FILE__));
 if( is_admin() ){
-    require_once( TCM_ACC_PATH . 'process/wx-admin-settings.php' );
+    require_once( ATSEW_TCM_ACC_PATH . 'process/wx-admin-settings.php' );
 }
 
-require_once( TCM_ACC_PATH . 'process/wx-teachable.php' );
+require_once( ATSEW_TCM_ACC_PATH . 'process/wx-teachable.php' );
 
 
